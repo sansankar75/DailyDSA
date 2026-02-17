c2b5749936badbd4bbeb01472141c59571ca354d
sankara pandian <sankarapandian7575@gmail.com>
2026-02-17 22:23:47 +0530

    day one is good with confusions

--- tree.java
+++ tree.java
@@ -1,18 +1,60 @@
 class tree{
 
-    private static String nodes(String a,int l,String c)
+
+    /// its a string reverse using the recursion 
+    public static String reverse(String text, int i)
     {
-      if(l==0){
-        return c;
+      if(i<0){
+        return " ";
       }
-      return c + nodes(a,l-1,(String) a.charAt(l-1));
+      return text.charAt(i)+reverse(text,i-1);
     }
+
+    /// its a word reverse using the recursion     "I love coding" ===> "coding love I"
+
+
+    /* i have a method 
+    
+    i need to split the based on the space 
+    
+    i 
+    love 
+    coding 
+     
+     parameter (text,start,last)
+     sting text ="I love coding"
+     int len=text.len (13)
+    
+    base condition
+    if len<0 => return " "
+    else if len==" "
+         return text.substring(start,last)+fu
+
+    if len==maxlen */                                               
+    public static String reverse_word(String text,int start, int last)
+    {
+      if(start<0)
+      {
+        return text.substring(start+1,last+1);
+      }
+      else if(text.charAt(start)==' ')
+      {
+        //System.out.println(text.substring(start,last+1)+" its  a function");
+        return text.substring(start,last+1) + reverse_word(text,start-1,start-1);
+      }
+      else{
+        return reverse_word(text,start-1,last);
+      }
+    }
+
 
     public static void main(String args[])
     {
-        String value="san";
-        int lenOfValue=value.length();
-        String val=nodes(value,lenOfValue,value.chatAt(lenOfValue-1));
-        System.out.println(val);
+      String val="b    a";
+      int len=val.length()-1;
+      System.out.println(reverse_word(val,len,len));
+      ///System.out.println(reverse(val,val.length()-1));
+      //System.out.println("textsafd".substring(2,4));
+        
     }
 }
