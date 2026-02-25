1fa19f6b21c451298ecaba7074953790e71b75c5
sankara pandian <sankarapandian7575@gmail.com>
2026-02-25 20:43:06 +0530

    today is basic in recursion

---
+++ rec.java
@@ -0,0 +1,88 @@
+public class rec{
+
+    // print number from one to n  
+    private static int recursion_1_n(int value)
+    {
+        
+        if(value==1)
+        {
+            System.err.println(value);        // base case return after value ==1 
+            return 1;
+        }
+        int sum=recursion_1_n(value-1)+1;    // i store and print value 
+
+        System.out.println(sum);
+
+        return sum;                        // return the previous function call in recursion 
+
+    }
+
+    // print the number from n to 1 
+    private static int recursion_n_1(int value)
+    {
+        if(value==1){
+            System.out.println(value);
+            return 0;
+        }
+        System.err.println(value);
+        return recursion_n_1(value-1);
+    }
+
+    //print the sum of n number 
+    private static int sum_of_n(int value)
+    {
+        if(value==1)
+        {
+            return 1;
+        }
+        return sum_of_n(value-1)+value;
+    }
+
+    private static int fact(int value)
+    {
+        if(value==1)
+        {
+            return 1;
+        }
+        return value*fact(value-1);
+    }
+
+    // print the power with recursion 
+    private static int power(int value,int power){
+        if(power==1){
+            return value;
+        }
+        return value*power(value,power-1);
+    }
+ // 2 power 3 2x2x2   
+    private static int fib(int value)
+    {
+        if(value==1 || value==0)
+        {
+            return 1;
+        }
+        return fib(value-1)+fib(value-2);
+    }
+
+//     2   1
+//     1   1(return)
+//     1(return) 
+
+
+    public static void main(String [] args)
+    {
+        //recursion_1_n(10);
+        //recursion_n_1(10);
+        //int sum=sum_of_n(4);
+        //System.out.println(sum);
+        //int result=fact(4);
+        //System.out.println(result);
+        //int pow=power(2, 4);
+        //System.out.println(pow);
+        System.out.println(fib(4));
+
+    }
+}
+
+//    3 2 1
+//    2  1+ 1 0
